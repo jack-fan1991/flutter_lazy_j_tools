f886fb97f5401155fe2743cafac016fe41262ff2
jack <jack11jack11jack@gmail.com>
2024-05-16 16:32:34 +0800

    add extensions

---
+++ lib/extension/iterable_helper.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+extension IterableHelper<E> on Iterable<E> {
+  T? doWhenFirst<T>(T Function(E first) action, {T? Function()? orElse}) {
+    for (E element in this) {
+      return action(element);
+    }
+    if (orElse != null) return orElse();
+    return null;
+  }
+}
+
+extension DuplicateOfIterable on Iterable {
+  bool get containsDuplicateValues => toSet().length != length;
+}
+
+extension InsertWidgetSeparator on Iterable<Widget> {
+  insertSeparator(Widget separator,
+      {castToList = true, bool skipHead = false, bool skipLast = false}) {
+    final result = <Widget>[separator];
+    if (skipHead) {
+      result.clear();
+    }
+    // print('[InsertWidgetSeparator] start :result : $result');
+    for (final element in this) {
+      // print('InsertWidgetSeparator] add element: $element');
+      result.add(element);
+      if (skipLast && element == last) break;
+      result.add(separator);
+      // print('InsertWidgetSeparator] add separator: $separator');
+      // print('InsertWidgetSeparator] start :result : $result');
+    }
+    return castToList ? result.toList() : result;
+  }
+}
+
+extension IterableFilterNull<T> on Iterable<T?> {
+  Iterable<T> filterNull() {
+    return where((element) => element != null).cast<T>();
+  }
+}
--- lib/flutter_lazy_j_tools.dart
+++ lib/flutter_lazy_j_tools.dart
@@ -5,3 +5,4 @@ export 'src/dialog_builder/dialog_builder.dart';
 export 'src/draggable_divider/draggable_divider.dart';
 export 'src/text_field/debounce_text_field.dart';
 export 'src/utils/stream/stream_disposable.dart';
+export '';
